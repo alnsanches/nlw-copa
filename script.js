632704811754ee65bfd796fdd57f265d398a7977
Aline Sanches <96800792+alnsanches@users.noreply.github.com>
2022-11-02 14:10:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.querySelector("#app").innerHTML = `
             <img class="logo" src="./assets/logo.svg" alt="Logo da NLW Copa">
         </header>  
             <main id="cards">
-                ${createCard("20/11", "domingo", createGame("qatar", "13:00", "Ecuador"))}
+                ${createCard("20/11", "domingo", createGame("qatar", "13:00", "ecuador"))}
                 ${createCard("21/11", "segunda", createGame("england", "10:00", "iran") + createGame("senegal", "13:00", "netherlands") + createGame("usa", "16:00", "wales"))}
                 ${createCard("22/11", "terça", createGame("argentina", "07:00", "saudi arabia") + createGame("denmark", "10:00", "tunisia") + createGame("mexico", "13:00", "poland") + createGame("france", "16:00", "australia"))}
                 ${createCard("23/11", "quarta", createGame("morocco", "07:00", "croatia") + createGame("germany", "10:00", "japan") + createGame("spain", "13:00", "costa rica") + createGame("belgium", "16:00", "canada"))}
